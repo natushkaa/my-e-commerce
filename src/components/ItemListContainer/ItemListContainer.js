877326be88e3bf779f65c95605e8b36f401b347f
Natalia Lamana <natalia.lamana98@gmail.com>
2021-09-29 19:44:49 -0300

    se agrega itemlistcontainer

---
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,16 @@
+import "./ItemListContainer.css"
+
+const ItemListContainer = (props) => {
+    
+    return (
+        <div>
+            <h1>{props.greeting} </h1>
+            
+     
+
+        </div>
+    )
+}
+
+
+export default ItemListContainer;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
 
 import "./NavBar.css"
 
+
 const NavBar = () => {
 
     return(
